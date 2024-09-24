408df57b3ff37fb18f19d463ca34580e4bf5a2ff
Son_Nguyen <son_nguyentruong@datahouse.com>
2024-09-25 00:27:43 +0700

    test

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -2,11 +2,16 @@ require("dotenv").config();
 
 module.exports = {
   dev: {
+    database: process.env.DB_NAME,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false 
+      }
   },
 };
